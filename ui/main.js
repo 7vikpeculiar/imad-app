3ad051ef93432d38d3e3b42d28993fdcba8908f3
7vikpeculiar <bugganasathviksanjeev1998@gmail.com>
2017-08-06 18:12:27 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,9 @@
-console.log('Loaded!');
+var button = document.getElementById('counter');
+var counter = 0;
+button.onClick = function()
+{
+    
+  counter += 1;
+  var span = document.getElementById("count");
+  span.innerHTML = counter.toString();
+};
